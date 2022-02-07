83d14279b70a48f687f60e0ac9194db48dc76aad
seveneleven337 <solidxnake_6@hotmail.com>
2022-02-07 23:28:03 +0200

    springboot import json

---
+++ CityExample/src/main/java/com/vamk/CityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vamk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityExampleApplication.class, args);
+	}
+
+}
---
+++ CityExample/src/main/java/com/vamk/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.vamk.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vamk.entity.City;
+
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+}
